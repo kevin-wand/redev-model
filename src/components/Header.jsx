6b03f1476cd050ce55cb2c236755fe3d1c45fc49
Kevin Wand <kevinwand@Kevins-Air.home>
2021-06-12 17:31:46 -0400

    Header cleaned up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,11 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
+      <div id="bg1"></div>
+      <div id="bg2"></div>
+      <div id="bg3"></div>
+      <div id="bg4"></div>
+      <div id="bg5"></div>
       <h2 className="header-title">RE Investment Model</h2>
     </div>
   );
