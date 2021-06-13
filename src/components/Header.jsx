4325dac9b60e1da3f5ba380469d11a1b2f709a63
Kevin Wand <kevinwand@Kevins-Air.home>
2021-06-13 12:46:01 -0400

    draft Header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
       <div id="bg3"></div>
       <div id="bg4"></div>
       <div id="bg5"></div>
+      <div id="bg6"></div>
       <h2 className="header-title">RE Investment Model</h2>
     </div>
   );
